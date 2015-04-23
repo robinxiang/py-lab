064c8d1bf49fe3f5cbac4d25190a7d56693bcb6a
xiangtao <dancelinux@gmail.com>
2015-04-23 16:49:43 +0800

    修改

---
+++ coursera-function-001.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Apr 23 10:58:11 2015
+Coursera课程-函数
+@author: xiangtao
+"""
+
+def jisuan_sanjiao_mianji(base,height):
+    mianji=base*height/2
+    return mianji
+    
+sanjiao1=jisuan_sanjiao_mianji(4,5)
+print sanjiao1
